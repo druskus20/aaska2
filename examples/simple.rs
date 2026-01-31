7d437acf456d5ab15192a844756c315cff2d4441
druskus20 <43417195+druskus20@users.noreply.github.com>
2026-01-31 16:11:20 +0100

    wip
    wip
    
    Maybe lets go back to a single parsing+generating stage.
    pulldown-cmark does not play nice with storing the iterator.
    Neither does salsa, which doesnt admit lifetimes.
    
    So, maybe let's do the following. Lets simply traverse the file and
    assets as we parse the file. salsa should take care of caching for
    subsequent runs. The path should be cannonicalized. We should not write
    the asset twice to disk - so we need detection for that. Maybe we still
    want to construct a graph, but since we need to do it at
    parse+generation time, we might as well use a list.
    
    The crux of it is that the hash requires the content of the file, and
    replacing the links of assets requires having generated the assets
    first. HTML does not have caching anyway - though I wonder how including
    a sort of preview "iframe" of another file would work.
    
    wip

--- examples/simple.rs
+++ examples/simple.rs
@@ -3,9 +3,10 @@ use aaska2::{Md, MdFile};
 fn main() {
     aaska2::init();
     let test_file = MdFile::new_from_str(
-        "# Hello, Aaska2!\nThis is a simple markdown file.",
-        "example.md",
+        "# Hello, Aaska2!\nThis is a simple markdown file.\n![An example image](image-old.png)\n",
+        std::path::PathBuf::from("test.md"),
     );
-    let chonk = aaska2::md_to_chonk(test_file);
+    let db = aaska2::db::LazyInputDatabase::default();
+    let chonk = aaska2::db::md_to_html(test_file);
     dbg!(chonk);
 }
